9cdbfcbba468b9aa2db536d5fae03513da6b9445
vevertluisant <hecoulon@gmail.com>
2020-07-22 00:19:45 +0200

    at_last

--- app.rb
+++ app.rb
@@ -4,15 +4,16 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-
+# on définit qui seront 2 joueurs avec la méthode initialize
 player1 = Player.new("Josiane")
-puts "Voici à ma droite la charcutière de Roubaix: #{player1.name}"
+puts "Voici à ma droite la charcutière de Roubaix: #{player1.name}" 
 
 player2 = Player.new("José")
 puts "Et à ma gauche le désosseur de Montargis: #{player2.name}"
 
 puts "========================================================"
-
+#ici, on vérifie bien si notre variable "santé" 
+#-- l'attr_accessor life_point-- est bien défini
 puts "Dans quelle forme se trouve nos combattants?"
 player1.show_state
 player2.show_state
@@ -21,26 +22,22 @@ puts "Fini de discuter! Maintenant, on casse des bouches! "
 
 puts "Honneur aux dames! "
 puts "========================================================="
+# ici commence la boucle qui va nous permettre de jalonner
+# notre combat. Celui s'arrête dès qu'un des combattants voit sa vie tomber à zéro
+while player1.life_points > 0 && player2.life_points > 0                                                         
+puts player1.attacks(player2)
+puts player2.gets_damage(player1.compute_damage)
 
-while player1.life_points > 0 && player2.life_points >0 
-    puts "#{player1.name} attaque #{player2.name}"
-    puts player2.gets_damage(player1.compute_damage)
-    puts "#{player2.name} attaque #{player1.name}"
-    puts player1.gets_damage(player2.compute_damage)
-    if player2.life_points <1 
-    elsif player1.life_points <1
-      break
-    end
+if player2.life_points > 0
+  puts player2.attacks(player1)
+  puts player1.gets_damage(player2.compute_damage)
+end
 
-puts "========================================================="
-puts "Voyons maintenant comment vont nos champions"
+puts '========================================================='
+puts 'Voyons maintenant comment vont nos champions'
 puts player1.show_state
 puts player2.show_state
 end
-
-
-
-
 
 
 binding.pry
--- lib/player.rb
+++ lib/player.rb
@@ -1,20 +1,25 @@
 class Player
 attr_accessor :name, :life_points
 @@all_players = []
+
+# méthode "mère" qui va initialiser tous les personnages
 def initialize(name)
     @name = name
     @life_points = 10
     @@all_players << self
 end
+
+#méthode pour définir les PV de chaque joueur
 def show_state
     puts "#{@name} a #{@life_points} points de vie"
 end
 
-
+#méthode pour convoquer tous les joueurs
 def self.all
     return @@all_players
 end
 
+#méthode pour compter les dommages subis
 def gets_damage(hit)
     @life_points = @life_points - hit
     if @life_points <= 0
@@ -23,13 +28,15 @@ def gets_damage(hit)
     end
 end
 
+#métode d'attaque
 def attacks(player)
     puts "#{name} attaque #{player.name}"
 end
 
+#méthode jet de dé
 def compute_damage
     return rand(1..6)
-  end
+  end  
 end
  
 
